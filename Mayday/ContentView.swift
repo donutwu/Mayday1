2d825bd18241592a98ccfac07b25095ad23c027a
DolosMars <high106huiyu@gmail.com>
2022-10-25 12:54:33 +0800

    Initial Commit

---
+++ Mayday/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mayday
+//
+//  Created by User06 on 2022/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
